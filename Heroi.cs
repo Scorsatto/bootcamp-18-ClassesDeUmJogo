45be830e25f7644cae51d63b9fac11707058af36
Scorsatto <fabianoscorsatto@gmail.com>
2024-12-09 14:26:05 -0400

    Adicionar arquivos de projeto.

---
+++ Heroi.cs
@@ -0,0 +1,35 @@
+﻿namespace bootcamp_18_ClassesDeUmJogo
+{
+    internal class Heroi
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Tipo { get; set; }
+
+        public void Atacar()
+        {
+            string ataque = "";
+
+            switch (Tipo)
+            {
+                case "guerreiro":
+                    ataque = "espada";
+                    break;
+
+                case "mago":
+                    ataque = "magia";
+                    break;
+
+                case "monge":
+                    ataque = "artes marciais";
+                    break;
+
+                case "ninja":
+                    ataque = "shuriken";
+                    break;
+            }
+
+            Console.WriteLine($"o {Tipo} atacou usando {ataque}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using bootcamp_18_ClassesDeUmJogo;
+
+Heroi heroi = new Heroi();
+
+Console.WriteLine("Informe o nome do heroi");
+heroi.Nome = Console.ReadLine();
+
+Console.WriteLine("Informe a idade do heroi");
+heroi.Idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe a classe do heroi");
+heroi.Tipo = Console.ReadLine();
+
+heroi.Atacar();
+
+
